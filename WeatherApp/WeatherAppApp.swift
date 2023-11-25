481bb085b440bc508916892f9c34af24f8a8cae3
j-balkovec <jakob.balkovec@gmail.com>
2023-11-24 17:09:24 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jakob Balkovec on 24. 11. 23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
