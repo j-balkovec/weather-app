8245bb30f386ab101c33b04803894fe7e0e6d84c
j-balkovec <jakob.balkovec@gmail.com>
2024-02-09 17:39:41 -0800

    restructure

---
+++ WeatherApp/Views/ContentView 2.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Jakob Balkovec on 24. 11. 23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            WelcomeView()
+                .environmentObject(locationManager)
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Jakob Balkovec on 24. 11. 23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .padding()
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
